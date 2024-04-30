c162c436e05d952eaf354b61b08e95e776a5bf47
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:01:10 +0200

    feat: Created Wine entity model

---
+++ VineyardSite/VineyardSite/Model/Wine.cs
@@ -0,0 +1,12 @@
+﻿namespace VineyardSite.Model;
+
+public class Wine
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public double AlcoholContent { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    
+}
