97674ca4d6784aab036de2685ee88ce1b35b9f90
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:02:07 +0200

    feat: created Order entity model

---
+++ VineyardSite/VineyardSite/Model/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace VineyardSite.Model;
+
+public class Order
+{
+    public Guid Id { get; set; }
+    public Wine Wine { get; set; }
+    public int WineId { get; set; }
+    public int Quantity { get; set; }
+    public double TotalPrice { get; set; }
+    public DateTime Date { get; set; }
+    public string Address { get; set; }
+    public string DeliveryType { get; set; }
+    public string PaymentType { get; set; }
+    public string Status { get; set; }
+    public string Notes { get; set; }
+    public string Email { get; set; }
+    public User User { get; set; }
+    public string UserId { get; set; }
+    
+}
