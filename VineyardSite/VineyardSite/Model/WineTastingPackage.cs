762432bf3143fa1476e37528a3dd1d8a320b8d8d
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:02:53 +0200

    feat: Created WineTastingPackage entity model

---
+++ VineyardSite/VineyardSite/Model/WineTastingPackage.cs
@@ -0,0 +1,19 @@
+﻿namespace VineyardSite.Model;
+
+public class WineTastingPackage
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public Wine Wine1 { get; set; }
+    public Wine? Wine2 { get; set; }
+    public Wine? Wine3 { get; set; }
+    public Wine? Wine4 { get; set; }
+    public int Wine1Id { get; set; }
+    public int? Wine2Id { get; set; }
+    public int? Wine3Id { get; set; }
+    public int? Wine4Id { get; set; }
+    public Snack? Snack { get; set; }
+    public int? SnackId { get; set; }
+}
