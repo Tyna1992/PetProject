74f807b72c7c5282b64563dbe242714f9bcc1fd3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:02:26 +0200

    feat: Created Appointment entity model

---
+++ VineyardSite/VineyardSite/Model/Appointment.cs
@@ -0,0 +1,15 @@
+﻿namespace VineyardSite.Model;
+
+public class Appointment
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int People { get; set; }
+    public string Name { get; set; }
+    public string Requests { get; set; }
+    public string Email { get; set; }
+    public User User { get; set; }
+    public string UserId { get; set; }
+    public WineTastingPackage Package { get; set; }
+    public int PackageId { get; set; }
+}
