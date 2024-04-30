77027c73ec791518eaa6160952e2b7718259a51f
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:01:49 +0200

    feat: Created Snack entity model

---
+++ VineyardSite/VineyardSite/Model/Snack.cs
@@ -0,0 +1,10 @@
+﻿namespace VineyardSite.Model;
+
+public class Snack
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    
+}
