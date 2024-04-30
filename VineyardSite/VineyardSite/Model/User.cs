c6440d6c5c1fb6515c639ffedc7c26b5b50452f9
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-04-30 13:01:34 +0200

    feat: Created User entity model

---
+++ VineyardSite/VineyardSite/Model/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace VineyardSite.Model;
+
+public class User : IdentityUser
+{
+    [Required]
+    public string Address { get; set; }
+    
+    
+}
