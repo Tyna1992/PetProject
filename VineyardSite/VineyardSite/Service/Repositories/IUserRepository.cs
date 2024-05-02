3d02703999bb01d32ddda22e0845bdf5bb63662a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-02 13:40:54 +0200

    feat: Repository layer created for User entity with CRUD operations

---
+++ VineyardSite/VineyardSite/Service/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User> GetUserById(string id);
+    Task<User> GetByEmail(string email);
+    Task UpdateUser(string id, User user);
+    Task DeleteUser(string id);
+    
+}
---
+++ VineyardSite/VineyardSite/Service/Repositories/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardSite.Data;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly UserContext _context;
+    
+    public UserRepository(UserContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<User>> GetAllUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<User> GetUserById(string id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+    
+    public async Task<User> GetByEmail(string email)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(user => user.Email == email);
+        return user;
+    }
+    
+    public async Task UpdateUser(string id, User user)
+    {
+        var userToUpdate = await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+        if (userToUpdate != null)
+        {
+            userToUpdate.UserName = user.UserName;
+            userToUpdate.Email = user.Email;
+            userToUpdate.Address = user.Address;
+            _context.Users.Update(userToUpdate);
+            await _context.SaveChangesAsync();
+        }
+    }
+    
+    public async Task DeleteUser(string id)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(user => user.Id == id);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
