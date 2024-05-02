c294eb690d11a74e052b0354fa5719ecb164db88
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-02 13:45:22 +0200

    feat: Repository layer created for Snack entity with CRUD operations

---
+++ VineyardSite/VineyardSite/Service/Repositories/ISnackRepository.cs
@@ -0,0 +1,13 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface ISnackRepository
+{
+    Task<IEnumerable<Snack>> GetAllSnacks();
+    Task<Snack> GetSnackById(int id);
+    Task AddSnack(Snack snack);
+    Task UpdateSnack(int id, Snack snack);
+    Task DeleteSnack(int id);
+    
+}
---
+++ VineyardSite/VineyardSite/Service/Repositories/SnackRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardSite.Data;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public class SnackRepository : ISnackRepository
+{
+    private readonly DataContext _context;
+    
+    public SnackRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<Snack>> GetAllSnacks()
+    {
+        return await _context.Snacks.ToListAsync();
+    }
+
+    public async Task<Snack> GetSnackById(int id)
+    {
+        var snack = await _context.Snacks.FindAsync(id);
+        return snack;
+    }
+
+    public async Task AddSnack(Snack snack)
+    {
+        _context.Snacks.Add(snack);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateSnack(int id, Snack snack)
+    {
+        var snackToUpdate = await _context.Snacks.FirstOrDefaultAsync(snack => snack.Id == id);
+        if (snackToUpdate != null)
+        {
+            snackToUpdate.Name = snack.Name;
+            snackToUpdate.Price = snack.Price;
+            snackToUpdate.Description = snack.Description;
+            _context.Snacks.Update(snackToUpdate);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteSnack(int id)
+    {
+        var snack = await _context.Snacks.FirstOrDefaultAsync(snack => snack.Id == id);
+        if (snack != null)
+        {
+            _context.Snacks.Remove(snack);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
