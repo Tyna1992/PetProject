9214682a48f2dc7f78ec98468290da579205f8f6
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-03 15:23:51 +0200

    feat: Controller for Snacks created with POST endpoint

---
+++ VineyardSite/VineyardSite/Controllers/SnackController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VineyardSite.Model;
+using VineyardSite.Service.Repositories;
+
+namespace VineyardSite.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class SnackController : ControllerBase
+{
+    private readonly ISnackRepository _snackRepository;
+    
+    public SnackController(ISnackRepository snackRepository)
+    {
+        _snackRepository = snackRepository;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> AddSnack([FromBody]Snack snack)
+    {
+        try
+        {
+            await _snackRepository.AddSnack(snack);
+            return Ok(snack);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+}
