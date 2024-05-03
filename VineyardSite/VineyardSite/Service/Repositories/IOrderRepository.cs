4ebdd73fd0ae1efb4196e2cd4d59324ebd3f6a36
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-03 14:41:03 +0200

    feat: Repository layer created for Order entity with CRUD operations

---
+++ VineyardSite/VineyardSite/Service/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface IOrderRepository
+{
+    Task<IEnumerable<Order>> GetAllOrders();
+    Task<Order> GetOrderById(string id);
+    Task AddOrder(Order order);
+    Task UpdateOrder(string id, Order order);
+    Task DeleteOrder(string id);
+}
---
+++ VineyardSite/VineyardSite/Service/Repositories/OrderRepository.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardSite.Data;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly DataContext _context;
+    
+    public OrderRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<Order>> GetAllOrders()
+    {
+        return await _context.Orders.ToListAsync();
+    }
+    
+    public async Task<Order> GetOrderById(string id)
+    {
+        var order = await _context.Orders.FindAsync(id);
+        return order;
+    }
+    
+    public async Task AddOrder(Order order)
+    {
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task UpdateOrder(string id, Order order)
+    {
+        var orderToUpdate = await _context.Orders.FirstOrDefaultAsync(order => order.Id.ToString() == id);
+        if (orderToUpdate != null)
+        {
+            orderToUpdate.Date = order.Date;
+            orderToUpdate.TotalPrice = order.TotalPrice;
+            orderToUpdate.User = order.User;
+            orderToUpdate.Wine = order.Wine;
+            orderToUpdate.Address = order.Address;
+            orderToUpdate.Quantity = order.Quantity;
+            orderToUpdate.DeliveryType = order.DeliveryType;
+            orderToUpdate.PaymentType = order.PaymentType;
+            orderToUpdate.Status = order.Status;
+            orderToUpdate.Notes = order.Notes;
+            orderToUpdate.Email = order.Email;
+            _context.Orders.Update(orderToUpdate);
+            await _context.SaveChangesAsync();
+        }
+    }
+    
+    public async Task DeleteOrder(string id)
+    {
+        var order = await _context.Orders.FirstOrDefaultAsync(order => order.Id.ToString() == id);
+        if (order != null)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
