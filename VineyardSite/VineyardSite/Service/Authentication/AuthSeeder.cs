0eba430f26085e422eb47e6bfc351b97f84d782a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-03 15:22:15 +0200

    feat: authentication seeder implemented

---
+++ VineyardSite/VineyardSite/Service/Authentication/AuthSeeder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Authentication;
+
+public class AuthSeeder
+{
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<User> userManager;
+
+    public AuthSeeder(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+    {
+        this._roleManager = roleManager;
+        this.userManager = userManager;
+    }
+
+    public void AddRoles()
+    {
+        var tAdmin = CreateAdminRole(_roleManager);
+        tAdmin.Wait();
+
+        var tUser = CreateUserRole(_roleManager);
+        tUser.Wait();
+    }
+
+    public void AddAdmin()
+    {
+        var tAdmin = CreateAdminIfNotExists();
+        tAdmin.Wait();
+    }
+
+    private async Task CreateAdminIfNotExists()
+    {
+        var adminInDb = await userManager.FindByEmailAsync("admin@admin.com");
+        if (adminInDb == null)
+        {
+            var admin = new User { UserName = "admin", Email = "admin@admin.com", Address = "admin"};
+            var adminCreated = await userManager.CreateAsync(admin, "admin");
+
+            if (adminCreated.Succeeded)
+            {
+                await userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+
+    private async Task CreateAdminRole(RoleManager<IdentityRole> roleManager)
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin")); 
+    }
+
+    async Task CreateUserRole(RoleManager<IdentityRole> roleManager)
+    {
+        await roleManager.CreateAsync(new IdentityRole("User")); 
+    }
+}
