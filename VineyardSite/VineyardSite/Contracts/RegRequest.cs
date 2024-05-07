26130d38e525bb546a65a038eb1981850b9e9212
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-07 15:14:19 +0200

    bugfix: Registration request properties corrected

--- VineyardSite/VineyardSite/Contracts/RegRequest.cs
+++ VineyardSite/VineyardSite/Contracts/RegRequest.cs
@@ -1,9 +1,10 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace HealthManagerServer.Contracts;
+
 public record RegistrationRequest(
-    [Required]string Email, 
-    [Required]string Username, 
-    [Required]string Password,
-    [Required]double Weight,
-    [Required]string Gender);
+    [Required] string Email,
+    [Required] string Username,
+    [Required] string Password,
+    [Required] string Address);
+    
