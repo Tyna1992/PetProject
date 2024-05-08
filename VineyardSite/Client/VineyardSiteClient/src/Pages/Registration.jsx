69cb6aa8cdd6ccf5d93326750cfebe3f0a7e6a22
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 21:10:13 +0200

    feat: user registration page created

---
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Registration.jsx
@@ -0,0 +1,80 @@
+﻿import {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {toast, ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import notify from '../Utils/Notify';
+import '../index.css';
+
+function RegisterUser() {
+    const navigate = useNavigate();
+    
+   
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const username = event.target.username.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const address = event.target.address.value;
+        const user = {username, email, password, address};
+        
+        try{
+            const response = await fetch("/api/Auth/Register",{
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+                
+            });
+            if(!response.ok){
+                throw new Error("Registration failed!");
+            }
+            notify("Registration successful!", "success");
+           
+            // const loginResponse = await fetch("/api/Auth/Login",{
+            //     method: "POST",
+            //     headers:{
+            //         "Content-Type": "application/json"
+            //     },
+            //     body: JSON.stringify({username, password})
+            // });
+            // if(!loginResponse.ok){
+            //     throw new Error("Login failed!");
+            // }
+            
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
+    
+    return(
+        <div className="registration">
+            <form className="regFrom" onSubmit={handleSubmit}>
+                <label>Username:</label>
+                <br></br>
+                <input type="text" name="username"></input>
+                <br></br>
+                <label>Email:</label>
+                <br></br>
+                <input type="email" name="email"></input>
+                <br></br>
+                <label>Password:</label>
+                <br></br>
+                <input type="password" name="password"></input>
+                <br></br>
+                <label>Address:</label>
+                <br></br>
+                <input type="text" name="address"></input>
+                <br></br>
+                <button type="submit">Create account</button>
+                <button type="button" onClick={() => navigate("/")}>
+                    Cancel
+                </button>
+            </form>
+            
+        </div>
+    )
+}
+
+export default RegisterUser;
