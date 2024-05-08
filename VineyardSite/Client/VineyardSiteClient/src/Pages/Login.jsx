865b17446cb83ad6bed6382d6da48b444d99c3ce
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 21:55:01 +0200

    feat: login logic implemented on the frontend

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Login.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Login.jsx
@@ -1,5 +1,7 @@
 ﻿import {useState} from "react";
 import { useNavigate } from "react-router-dom";
+import "../index.css";
+import notify from "../Utils/Notify";
 
 function Login() 
 {
@@ -7,11 +9,40 @@ function Login()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("/api/Auth/Login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({username, password})
+            });
+            if (!response.ok) {
+                notify("Invalid username or password!", "error");
+                throw new Error("Invalid credentials");
+            }
+            setPassword("");
+            setUsername("");
+            notify("Login successful!", "success");
+            setTimeout(() => {
+                navigate("/");
+            }, 3100);
+            
+
+
+        } catch (error) {
+            notify("Login failed!", "error");
+            console.error(error);
+        }
+    }
+    
     return(
         <div className="login">
             <h1>Login</h1>
-            <div className="container">
-                <form>
+            <div className="loginForm">
+                <form onSubmit={handleSubmit}>
                     <label>
                         Username:
                     </label>
@@ -22,11 +53,11 @@ function Login()
                         Password:
                     </label>
                     <br/>
-                    <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     <br/>
                     <button type="submit">Login</button>
                     <button onClick={() => navigate("/")}>Cancel</button>                   
-                       <button/> 
+                      
                 </form>
             </div>
         </div>
