45ca4d9796e7bc25ca130061f0ff954858fcaec7
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 21:33:00 +0200

    feat: Login form created

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Login.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Login.jsx
@@ -1,7 +1,36 @@
 ﻿import {useState} from "react";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+function Login() 
+{
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     
+    return(
+        <div className="login">
+            <h1>Login</h1>
+            <div className="container">
+                <form>
+                    <label>
+                        Username:
+                    </label>
+                    <br/>
+                    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <br/>
+                    <label>
+                        Password:
+                    </label>
+                    <br/>
+                    <input type="text" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <br/>
+                    <button type="submit">Login</button>
+                    <button onClick={() => navigate("/")}>Cancel</button>                   
+                       <button/> 
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Login;
