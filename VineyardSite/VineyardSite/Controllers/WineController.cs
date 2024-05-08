73effed15948d007ea7a3808b6762bd72ae4265b
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 13:23:46 +0200

    feat: controller for wine created with POST endpoint

---
+++ VineyardSite/VineyardSite/Controllers/WineController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VineyardSite.Model;
+using VineyardSite.Service.Repositories;
+
+namespace VineyardSite.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class WineController : ControllerBase
+{
+    private readonly IWineRepository _wineRepository;
+    
+    public WineController(IWineRepository wineRepository)
+    {
+        _wineRepository = wineRepository;
+    }
+
+    [HttpPost("/wine/AddWine"), Authorize(Roles = "Admin")]
+    public async Task<IActionResult> AddWine([FromBody] Wine wine)
+    {
+        await _wineRepository.AddWine(wine);
+        return Ok(wine);
+    }
+    
+    
+}
