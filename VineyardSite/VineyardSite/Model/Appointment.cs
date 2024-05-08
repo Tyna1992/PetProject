94fed24e70c47bdecd132e82dced52dc11f09865
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 13:17:07 +0200

    fix: Appointment Requests property can be null

--- VineyardSite/VineyardSite/Model/Appointment.cs
+++ VineyardSite/VineyardSite/Model/Appointment.cs
@@ -6,7 +6,7 @@ public class Appointment
     public DateTime Date { get; set; }
     public int People { get; set; }
     public string Name { get; set; }
-    public string Requests { get; set; }
+    public string? Requests { get; set; }
     public string Email { get; set; }
     public User User { get; set; }
     public string UserId { get; set; }
