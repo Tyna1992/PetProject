3ea8a5da237039059e026b35fbc64c8847d8d840
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 13:56:37 +0200

    feat: client side routing implemented with react-router-dom

--- VineyardSite/Client/VineyardSiteClient/src/main.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './Home.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<Home/>,
+        children:[
+            
+        ]
+    }
+]);
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 )
