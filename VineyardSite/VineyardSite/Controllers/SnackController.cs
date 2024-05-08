e41fd2b8da5e147bc3e0b0125981fca40ee3a77a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-08 13:23:09 +0200

    feat: Template added to POST endpoint, DELETE endpoint added

--- VineyardSite/VineyardSite/Controllers/SnackController.cs
+++ VineyardSite/VineyardSite/Controllers/SnackController.cs
@@ -16,13 +16,55 @@ public class SnackController : ControllerBase
         _snackRepository = snackRepository;
     }
     
-    [HttpPost]
+    [HttpPost("/snack/AddSnack")]
     public async Task<IActionResult> AddSnack([FromBody]Snack snack)
     {
         try
         {
             await _snackRepository.AddSnack(snack);
             return Ok(snack);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpDelete("/snack/DeleteSnack/{id:int}")]
+    public async Task<IActionResult> DeleteSnack(int id)
+    {
+        try
+        {
+            await _snackRepository.DeleteSnack(id);
+            return Ok("Snack deleted");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet("/snack/GetSnackById/{id:int}")]
+    public async Task<IActionResult> GetSnackById(int id)
+    {
+        try
+        {
+            var snack = await _snackRepository.GetSnackById(id);
+            return Ok(snack);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet("/snack/GetAllSnacks")]
+    public async Task<IActionResult> GetAllSnacks()
+    {
+        try
+        {
+            var snacks = await _snackRepository.GetAllSnacks();
+            return Ok(snacks);
         }
         catch (Exception e)
         {
