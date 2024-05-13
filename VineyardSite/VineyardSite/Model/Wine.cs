ed955f29b4ef35abc3358407b023d136541f66be
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-13 11:27:52 +0200

    fix: model changed to function as a catalog

--- VineyardSite/VineyardSite/Model/Wine.cs
+++ VineyardSite/VineyardSite/Model/Wine.cs
@@ -5,8 +5,7 @@ public class Wine
     public int Id { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
-    public double AlcoholContent { get; set; }
-    public double Price { get; set; }
     public string Description { get; set; }
+    
     
 }
