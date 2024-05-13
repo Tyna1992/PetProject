60511acefdf30ab60f38f14de6a04f1c8fcee3a5
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-13 11:29:01 +0200

    feat: Model created to track the stock of the wines

---
+++ VineyardSite/VineyardSite/Model/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace VineyardSite.Model;
+
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public int WineVariantId { get; set; }
+    public WineVariant WineVersion { get; set; }
+    public int Quantity { get; set; }
+}
