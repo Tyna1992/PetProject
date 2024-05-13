917fe0020a42e5dcdb226542e55dbcf318c18b9b
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-13 11:28:27 +0200

    feat: Model created to track all the variants of the same wine

---
+++ VineyardSite/VineyardSite/Model/WineVariant.cs
@@ -0,0 +1,11 @@
+﻿namespace VineyardSite.Model;
+
+public class WineVariant
+{
+    public int Id { get; set; }
+    public Wine Wine { get; set; }
+    public int WineId { get; set; }
+    public double AlcoholContent { get; set; }
+    public double Price { get; set; }
+    public int Year { get; set; }
+}
