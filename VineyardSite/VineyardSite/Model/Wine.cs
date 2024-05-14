a1c2ae9981e214311b8bf7810cfd878e71672eb5
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-14 10:50:53 +0200

    fix: Sweetness property added to Wine entity

--- VineyardSite/VineyardSite/Model/Wine.cs
+++ VineyardSite/VineyardSite/Model/Wine.cs
@@ -5,6 +5,7 @@ public class Wine
     public int Id { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public string Sweetness { get; set; }
     public string Description { get; set; }
     
     
