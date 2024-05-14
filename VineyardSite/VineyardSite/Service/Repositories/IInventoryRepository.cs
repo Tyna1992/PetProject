8fc41cf2b4cbffe05efb674d91d11d7162a7fe03
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-14 10:54:31 +0200

    feat: Repository layer added for InventoryItem entity

---
+++ VineyardSite/VineyardSite/Service/Repositories/IInventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface IInventoryRepository
+{
+    Task AddInventoryItem(InventoryItem inventoryItem);
+    Task<IEnumerable<InventoryItem>> GetAllInventoryItems();
+    Task<InventoryItem?> GetInventoryItem(int id);
+    Task AddStock(int id, int quantity);
+    Task RemoveStock(int id, int quantity);
+    Task DeleteInventoryItem(int id);
+}
---
+++ VineyardSite/VineyardSite/Service/Repositories/InventoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardSite.Data;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public class InventoryRepository : IInventoryRepository
+{
+    private readonly DataContext _context;
+    
+    public InventoryRepository(DataContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task AddInventoryItem(InventoryItem inventoryItem)
+    {
+        _context.Inventory.Add(inventoryItem);
+        await _context.SaveChangesAsync();
+       
+    }
+
+    public async Task<IEnumerable<InventoryItem>> GetAllInventoryItems()
+    {
+        return await _context.Inventory.ToListAsync();
+    }
+
+    public async Task<InventoryItem?> GetInventoryItem(int id)
+    {
+        return await _context.Inventory.FirstOrDefaultAsync(inventoryItem => inventoryItem.Id == id);
+    }
+
+    public async Task AddStock(int id, int quantity)
+    {
+        var inventoryItem = await _context.Inventory.FirstOrDefaultAsync(inventoryItem => inventoryItem.Id == id);
+        if (inventoryItem != null)
+        {
+            inventoryItem.Quantity += quantity;
+            _context.Inventory.Update(inventoryItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task RemoveStock(int id, int quantity)
+    {
+        var inventoryItem = await _context.Inventory.FirstOrDefaultAsync(inventoryItem => inventoryItem.Id == id);
+        if (inventoryItem != null)
+        {
+            inventoryItem.Quantity -= quantity;
+            _context.Inventory.Update(inventoryItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteInventoryItem(int id)
+    {
+        var inventoryItem = await _context.Inventory.FirstOrDefaultAsync(inventoryItem => inventoryItem.Id == id);
+        if (inventoryItem != null)
+        {
+            _context.Inventory.Remove(inventoryItem);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
