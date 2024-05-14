500b675ed246b62035fa98149a695a4451ad2cc3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-14 10:57:59 +0200

    feat: Controller for handle inventory created

---
+++ VineyardSite/VineyardSite/Controllers/InventoryController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VineyardSite.Model;
+using VineyardSite.Service.Repositories;
+
+namespace VineyardSite.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class InventoryController : ControllerBase
+{
+    private readonly IInventoryRepository _inventoryRepository;
+    private readonly IWineVariantRepository _wineVariantRepository;
+    private readonly IWineRepository _wineRepository;
+    private ILogger<InventoryController> _logger;
+    
+    public InventoryController(IInventoryRepository inventoryRepository,
+        IWineVariantRepository wineVariantRepository, ILogger<InventoryController> logger,IWineRepository wineRepository)
+    {
+        _inventoryRepository = inventoryRepository;
+        _wineVariantRepository = wineVariantRepository;
+        _logger = logger;
+        _wineRepository = wineRepository;
+    }
+    
+    [HttpPost("AddInventory")]
+    public async Task<IActionResult> AddInventory(string name, int year,int quantity =1 )
+    {
+        var wine = await _wineRepository.GetWineByName(name);
+        if (wine == null)
+        {
+            return NotFound("No wine found with that name in catalog");
+        }
+        var variant = await _wineVariantRepository.GetVariantByYear(year);
+        
+        if (variant == null)
+        {
+            return NotFound("No variant found with that year");
+        }
+        var inventoryItem = new InventoryItem
+        {
+            WineVersion = variant,
+            WineVariantId = variant.Id,
+            Quantity = quantity
+        };
+        try
+        {
+            await _inventoryRepository.AddInventoryItem(inventoryItem);
+            return Ok(inventoryItem);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error adding inventory item");
+            return StatusCode(500);
+        }
+    }
+    
+    
+}
