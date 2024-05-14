0f51d3df6e2f559cb639e9673f24ed5924c2e8f3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-14 10:56:22 +0200

    feat: GET, DELETE and PUT endpoint created

--- VineyardSite/VineyardSite/Controllers/WineController.cs
+++ VineyardSite/VineyardSite/Controllers/WineController.cs
@@ -19,12 +19,55 @@ public class WineController : ControllerBase
     [HttpPost("AddWine"), Authorize(Roles = "Admin")]
     public async Task<IActionResult> AddWine([FromBody] Wine wine)
     {
-        
         try
         {
             await _wineRepository.AddWine(wine);
             return Ok(wine);
 
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpGet("GetAllWine"), Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetAllWine()
+    {
+        try
+        {
+            var wines = await _wineRepository.GetAllWine();
+            return Ok(wines);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+    
+    
+    [HttpDelete("DeleteWine/{id}"), Authorize(Roles= "Admin")]
+    public async Task<IActionResult> DeleteWine(int id)
+    {
+        try
+        {
+            await _wineRepository.DeleteWine(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            
+            return BadRequest(e.Message);
+        }
+    }
+    
+    [HttpPut("UpdateWine/{id}"), Authorize(Roles = "Admin")]
+    public async Task<IActionResult> UpdateWine(int id, [FromBody] Wine wine)
+    {
+        try
+        {
+            await _wineRepository.UpdateWine(id, wine);
+            return Ok(wine);
         }
         catch (Exception e)
         {
