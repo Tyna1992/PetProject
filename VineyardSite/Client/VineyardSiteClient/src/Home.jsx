b7bf8e42120153128285f60db8b64d11e53969fb
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-21 15:32:39 +0200

    fix: classname added to buttons

--- VineyardSite/Client/VineyardSiteClient/src/Home.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Home.jsx
@@ -1,7 +1,7 @@
 import {useState, useEffect} from 'react'
 import {Link, Outlet, useLocation} from 'react-router-dom'
 import './index.css'
-import LogoutButton from './Components/LogoutButton.jsx'
+import LogoutButton from './Components/LogoutButton.jsx';
 function Home() {
     const [user, setUser] = useState(null);
     const location = useLocation();
@@ -30,50 +30,50 @@ function Home() {
 
     return (
 
-        <div className="home">
+       
             <div className="Layout">
                 <nav>
                     <Link to="/">
-                    <img src="/Images/logo.jpg" alt="logo" className="image"/>
+                    <img src="/Images/logo.jpg" alt="logo" className="links"/>
                 </Link>
                     {user === null ? (
                         <>
                             <Link to="/register">
-                                <button type="button">Register</button>
+                                <button className="homeButton" type="button">Register</button>
                             </Link>
                             <Link to="/login">
-                                <button type="button">Login</button>
+                                <button className="homeButton" type="button">Login</button>
                             </Link>
                             <Link to="/webshop">
-                                <button type="button">Webshop</button>
+                                <button className="homeButton" type="button">Webshop</button>
                             </Link>
                             <Link to="/winetasting">
-                                <button type="button">Wine tasting</button>
+                                <button className="homeButton" type="button">Wine tasting</button>
                             </Link>
                             <Link to="/about">
-                                <button type="button">About us</button>
+                                <button className="homeButton" type="button">About us</button>
                             </Link>
                             <Link to="/contact">
-                                <button type="button">Contact</button>
+                                <button className="homeButton" type="button">Contact</button>
                             </Link>
                         </>
                     ) : user !== "admin" ? (
                         <>
-                            <Link to="/profile">
+                            <Link className="homeButton" to="/profile">
                                 <button type="button">Profile</button>
                             </Link>
 
                             <Link to="/webshop">
-                                <button type="button"> Webshop</button>
+                                <button className="homeButton" type="button"> Webshop</button>
                             </Link>
                             <Link to="/winetasting">
-                                <button type="button">Wine tasting</button>
+                                <button className="homeButton" type="button">Wine tasting</button>
                             </Link>
                             <Link to="/about">
-                                <button type="button">About us</button>
+                                <button className="homeButton" type="button">About us</button>
                             </Link>
                             <Link to="/contact">
-                                <button type="button">Contact</button>
+                                <button className="homeButton" type="button">Contact</button>
                             </Link>
                             <LogoutButton/>
 
@@ -81,19 +81,19 @@ function Home() {
                     ) : (
                         <>
                             <Link to="/admin">
-                                <button type="button">Admin site</button>
+                                <button className="homeButton" type="button">Admin site</button>
                             </Link>
                             <Link to="/webshop">
-                                <button type="button">Webshop</button>
+                                <button className="homeButton" type="button">Webshop</button>
                             </Link>
                             <Link to="/winetasting">
-                                <button type="button">Wine tasting</button>
+                                <button className="homeButton" type="button">Wine tasting</button>
                             </Link>
-                            <Link to="/about">
-                                <button type="button">About us</button>
+                            <Link  to="/about">
+                                <button className="homeButton" type="button">About us</button>
                             </Link>
                             <Link to="/contact">
-                                <button type="button">Contact</button>
+                                <button className="homeButton" type="button">Contact</button>
                             </Link>
                             <LogoutButton/>
                         </>
@@ -102,7 +102,7 @@ function Home() {
                 </nav>
                 <Outlet/>
             </div>
-            </div>
+            
 
     )
 }
