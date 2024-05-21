c5cb9edc422c8666fa7fef8a62ecce45175128a4
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-21 15:33:25 +0200

    fix: unnecessary endpoints deleted

--- VineyardSite/VineyardSite/Controllers/OrderController.cs
+++ VineyardSite/VineyardSite/Controllers/OrderController.cs
@@ -22,53 +22,7 @@ public class OrderController : ControllerBase
     
     
     
-    [HttpPost("/order/AddOrder")]
-    public async Task<IActionResult> AddOrder(string username, int wineId,int quantity, DateTime date, string deliveryType, string paymentType,  string? notes)
-    {
-        var user = await _userRepository.GetByUsername(username);
-        if (user == null)
-        {
-            return NotFound("User not found. Please register/login first");
-        }
-        var wine = await _wineRepository.GetWineById(wineId);
-        if (wine == null)
-        {
-            return NotFound("Wine not found");
-        }
-        var order = new Order
-        {
-            Wine = wine,
-            WineId = wine.Id,
-            Quantity = quantity,
-            TotalPrice = wine.Price * quantity,
-            Date = date,
-            Address = user.Address,
-            DeliveryType = deliveryType,
-            PaymentType = paymentType,
-            Status = "Pending",
-            Notes = notes,
-            Email = user.Email,
-            User = user,
-            UserId = user.Id
-            
-        };
-        await _orderRepository.AddOrder(order);
-        return Ok(order);
-    }
     
-    [HttpDelete("/order/DeleteOrder/{id}"), Authorize(Roles = "Admin")]
-    public async Task<IActionResult> DeleteOrder(string id)
-    {
-        try
-        {
-            await _orderRepository.DeleteOrder(id);
-            return Ok("Order deleted");
-        }
-        catch (Exception e)
-        {
-            return BadRequest(e.Message);
-        }
-    }
        
 }
     
--- VineyardSite/VineyardSite/Controllers/WineTastingPackageController.cs
+++ VineyardSite/VineyardSite/Controllers/WineTastingPackageController.cs
@@ -21,43 +21,7 @@ public class WineTastingController : ControllerBase
         _snackRepository = snackRepository;
     }
 
-    [HttpPost("/wineTasting/AddPackage"), Authorize(Roles = "Admin")]
-    public async Task<IActionResult> AddPackage(string name, string description, double price,int wineId,int? snackId, int? wine2Id, int? wine3Id, int? wine4Id)
-    {
-       
-            var wine = await _wineRepository.GetWineById(wineId);
-            if(wine == null)
-            {
-                return NotFound("Wine not found");
-            }
-            var wine2 = await _wineRepository.GetWineById(wine2Id);
-            
-            var wine3 = await _wineRepository.GetWineById(wine3Id);
-           
-            var wine4 = await _wineRepository.GetWineById(wine4Id);
-          
-            var snack = await _snackRepository.GetSnackById(snackId);
-            
-            var package = new WineTastingPackage
-            {
-                Name = name,
-                Description = description,
-                Price = price,
-                Wine1 = wine,
-                Wine1Id = wine.Id,
-                Wine2 = wine2,
-                Wine2Id = wine2?.Id,
-                Wine3 = wine3,
-                Wine3Id = wine3?.Id,
-                Wine4 = wine4,
-                Wine4Id = wine4?.Id,
-                Snack = snack,
-                SnackId = snack?.Id
-            };
-            await _wineTastingRepository.AddWineTastingPackage(package);
-            return Ok(package);
-        }
-       
+    
     }
     
        
