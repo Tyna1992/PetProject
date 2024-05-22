eb2e1aaa2c86c54d7bc2bef40d6bfe87863b5372
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-22 10:27:00 +0200

    feat: WineForm component rendered on admin page
    Co-Authored-By: szoszo23 <szoszo23@users.noreply.github.com>

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Admin.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Admin.jsx
@@ -1,14 +1,13 @@
 ﻿import {useEffect, useState} from "react";
-import { useNavigate } from "react-router-dom";
 import TextareaAutosize from 'react-textarea-autosize';
 import "../index.css";
 import notify from "../Utils/Notify";
+import WineForm from "../Components/WineForm";
 
 
 function Admin()
 {
-    const navigate = useNavigate();
-    const [hideWineForm, setHideWineForm] = useState(true);
+
 
     
     
@@ -51,55 +50,7 @@ function Admin()
     
     return(
         <div className="admin">
-            <button type="button" onClick={()=> setHideWineForm(false)}>Add drink to the catalog</button>
-            
-            <div hidden={hideWineForm} className="wineForm">
-                <form onSubmit={addWines}>
-                    <label>
-                        Name:
-                    </label>
-                    <br/>
-                    <input type="text" name="wineName"  />
-                    <br/>
-                    <label>
-                        Type:
-                    </label>
-                    <br/>
-                    <select defaultValue={"DEFAULT"} name="type">
-                        <option value="DEFAULT" disabled  >Select please</option>
-                        <option value="Red">Red</option>
-                        <option value="White">White</option>
-                        <option value="Rose">Rose</option>
-                        <option value="Petnat">Pét-Nat</option>
-                        <option value="Non-alcohol">Non-Alcoholic</option>
-                        
-                    </select>
-                    <br/>
-                    <label>
-                        Sweetness:
-                    </label>
-                    <br/>
-                    <select defaultValue={"DEFAULT"}  name="sweetness">
-                        <option value="DEFAULT" disabled  >Select please</option>
-                        <option value="Dry">Dry</option>
-                        <option value="Semi-dry">Semi-Dry</option>
-                        <option value="Sweet">Sweet</option>
-                        <option value="Semi-sweet">Semi-Sweet</option>
-                        <option value="Non-alcohol">Non-Alcoholic</option>
-                        
-                    </select>
-                    <br/>
-                    <label>
-                        Description:
-                    </label>
-                    <br/>
-                    <TextareaAutosize className="textarea" name="description" />
-                    <br/>
-                    <button type="submit">Add</button>
-                    <button type="button" onClick={()=> setHideWineForm(true)}>Cancel</button>
-                    
-                </form>
-            </div>
+            <WineForm addWines={addWines} />
         </div>
     )
 }
