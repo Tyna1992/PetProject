db22542ec5d050f28ff702470ead6ff0251e74fd
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-22 13:46:21 +0200

    feat: inventory handling component used

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Admin.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Admin.jsx
@@ -4,6 +4,7 @@ import "../index.css";
 import notify from "../Utils/Notify";
 import WineForm from "../Components/WineForm";
 import WineVariantsForm from "../Components/WineVariantsForm";
+import InventoryForm from "../Components/InventoryForm";
 
 
 function Admin()
@@ -48,7 +49,7 @@ function Admin()
         }
         
     }
-
+    
     async function addWineVariants(event) {
         event.preventDefault();
         const name = event.target.name.value;
@@ -74,23 +75,62 @@ function Admin()
             })
             console.log(response.body);
             console.log(response);
-            if(!response.ok) {
-                notify("Failed to add vintage to the catalog", "error");
+            if(response.status === 404) {
+                notify("Failed to add vintage to the catalog, please check the name!", "error");
+                event.target.name.value = "";
+                event.target.price.value = "";
+                event.target.alcohol.value = "";
+                event.target.year.value = "";
+                throw new Error("Failed to add vintage to the catalog");
             }
             notify("Vintage added to the catalog", "success");
             event.target.name.value = "";
             event.target.price.value = "";
             event.target.alcohol.value = "";
             event.target.year.value = "";
-
-
     }
 
+    async function addStock(event)
+    {
+        event.preventDefault();
+        const wineName = event.target.wineName.value;
+        const year = parseInt(event.target.year.value);
+        const quantity = parseInt(event.target.quantity.value);
+        try {
+            const response = await fetch(`/api/Inventory/AddInventory/${wineName}/${year}/${quantity}`, {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    wineName: wineName,
+                    year: year,
+                    quantity: quantity
+                })
+            })
+            if(!response.ok)
+            {
+                notify("Failed to add stock", "error");
+                throw new Error("Failed to add stock");
+            }
+            notify("Stock added", "success");
+            event.target.wineName.value = "";
+            event.target.year.value = "";
+            event.target.quantity.value = "";
+
+        } catch (error) {
+            notify("An error occured adding stock", "error");
+            console.error(error);
+        }
+    }
     
     return(
-        <div className="admin">
+        <div className="admin-container">
             <WineForm addWines={addWines} />
             <WineVariantsForm addVinatge={addWineVariants}/>
+            <InventoryForm addStock={addStock} />
+
         </div>
     )
 }
