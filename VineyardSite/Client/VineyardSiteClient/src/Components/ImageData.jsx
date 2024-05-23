38905aebb1b11fa15fe25907cc2c59b46e6890bf
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-23 22:23:17 +0200

    fix: ImageData component contains images to display

--- VineyardSite/Client/VineyardSiteClient/src/Components/ImageData.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Components/ImageData.jsx
@@ -1,33 +1,65 @@
-﻿const images = [
+﻿import image2 from "../Images/image2.jpg";
+import image3 from "../Images/image3.jpg";
+import  image4 from "../Images/image4.jpg";
+import  image5 from "../Images/image5.jpg";
+import  image6 from "../Images/image6.jpg";
+import image7 from "../Images/image7.jpg";
+import image8 from "../Images/image8.jpg";
+import image9 from "../Images/image9.jpg";
+import image10 from "../Images/image10.jpg";
+import image11 from "../Images/image11.jpg";
+
+const images = [
+        
     {
         imgURL:
-            "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/254687895_4889290677761776_7877481935039052758_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=pyRo9_Rjn24Q7kNvgHu3fL1&_nc_oc=Adjy165aYQ7fKJUZXnShmeX4dnT1cw5JlsSm6YQRnoXtP3-ScIXL-Wbc8fZz5j4XDGw&_nc_ht=scontent-vie1-1.xx&oh=00_AYAd5nofM7g650Qaa_GhCvW7wNmHVN_vWKSI-vFWeSI9rQ&oe=66496D2F",
-        imgAlt: "img-1"
-    },
-    {
-        imgURL:
-            "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/394569191_801426691988033_1703634270286418447_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=cW6iK71d0L8Q7kNvgFPa6bY&_nc_ht=scontent-vie1-1.xx&oh=00_AYCcdbmJCJQ7O5QmrcfZuItLrmNt9jnzY2gtMBqqT-PTww&oe=6649661E",
+        image2,
         imgAlt: "img-2"
     },
     {
         imgURL:
-            "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/369654267_764541435676559_8981626222109841080_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=lzzEqFm_Z2IQ7kNvgGMe4Cm&_nc_ht=scontent-vie1-1.xx&oh=00_AYA7icZOar8O0oToj17rdJmbL-SZPgRQ5NTrb5Lr3-Z-7A&oe=664956AF",
+        image3,
         imgAlt: "img-3"
     },
     {
         imgURL:
-            "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/343929604_138052799159735_6086357164612459070_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LSERxGC8YAkQ7kNvgHhg8oN&_nc_ht=scontent-vie1-1.xx&oh=00_AYDXP6qT7GXQbbaEn-AmgfDN0Ck6RkgI6VS897S6PGNRVg&oe=66495E9A",
+        image4,
         imgAlt: "img-4"
     },
     {
-        imgURL: 
-            "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/280923480_5715486271798821_4512285047606357298_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=clORYo_e9dAQ7kNvgGMX2ti&_nc_ht=scontent-vie1-1.xx&oh=00_AYCZFCB9O6h1e61P-NlHDnLmu0Xu3WJk73dgpZB1d533PQ&oe=66496EE5",
+        imgURL:
+        image5,
         imgAlt: "img-5"
     },
     {
-        imgURL: 
-        "https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/309516217_518776210253084_8638170832818659936_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Q3p1iGbV6QoQ7kNvgFVK0VH&_nc_ht=scontent-vie1-1.xx&oh=00_AYCDg-0ua8YaOHyEteaooRXi-3_xAPSXs3Zz_TebqBdwQA&oe=66495AB5",
+        imgURL:
+        image6,
         imgAlt: "img-6"
+    },
+    {
+        imgURL:
+        image7,
+        imgAlt: "img-7"
+    },
+    {
+        imgURL:
+        image8,
+        imgAlt: "img-8"
+    },
+    {
+        imgURL:
+        image9,
+        imgAlt: "img-9"
+    },
+    {
+        imgURL:
+        image10,
+        imgAlt: "img-10"
+    },
+    {
+        imgURL:
+        image11,
+        imgAlt: "img-11"
     }
 ];
 
