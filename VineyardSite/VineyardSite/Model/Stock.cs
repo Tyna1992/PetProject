6a8e0e5b864b7772d13f51867a4bee93148ad54c
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-23 11:07:56 +0200

    feat:Stock model created to display full inventory
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>

---
+++ VineyardSite/VineyardSite/Model/Stock.cs
@@ -0,0 +1,10 @@
+
+public class Stock
+{
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public double Price { get; set; }
+    public double AlcoholContent { get; set; }
+    public int Quantity { get; set; }
+
+}
