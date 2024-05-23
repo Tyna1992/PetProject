4276c83a4da529c601f9e533649b78bddadf5bd5
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-23 22:24:17 +0200

    fix: imagedata array removed from carousel component

--- VineyardSite/Client/VineyardSiteClient/src/Components/CustomSlider.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Components/CustomSlider.jsx
@@ -1,10 +1,7 @@
 ﻿import { useState, useEffect } from "react";
-import image1 from "../Images/image1.jpg";
-import image2 from "../Images/image2.jpg";
-import image3 from "../Images/image3.jpg";
-import  image4 from "../Images/image4.jpg";
-import  image5 from "../Images/image5.jpg";
-import  image6 from "../Images/image6.jpg";
+
+
+
 
 import "./slider.css";
 
@@ -13,40 +10,9 @@ import "./slider.css";
 
 
 
-export default function About() {
+export default function About({images}) {
 
-    const images = [
-        {
-            imgURL:
-            image1,
-            imgAlt: "img-1"
-        },
-        {
-            imgURL:
-            image2,
-            imgAlt: "img-2"
-        },
-        {
-            imgURL:
-            image3,
-            imgAlt: "img-3"
-        },
-        {
-            imgURL:
-            image4,
-            imgAlt: "img-4"
-        },
-        {
-            imgURL:
-               image5,
-            imgAlt: "img-5"
-        },
-        {
-            imgURL:
-               image6,
-            imgAlt: "img-6"
-        }
-    ];
+    
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
