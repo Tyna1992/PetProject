06698aa87738837787d70a50c747bd70097567aa
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-27 13:40:12 +0200

    fix: Cart has reference to User

--- VineyardSite/VineyardSite/Model/Cart.cs
+++ VineyardSite/VineyardSite/Model/Cart.cs
@@ -3,6 +3,7 @@
 public class Cart
 {
     public int CartId { get; set; }
-    public ICollection<CartItem> CartItems { get; set; }
+    public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
     public string UserId { get; set; }
+    public User User { get; set; }
 }
