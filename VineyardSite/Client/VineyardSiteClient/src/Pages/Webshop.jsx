6398f0a4784992489f3b69118e15af2e427e4094
szoszo23 <szolnoki96@gmail.com>
2024-05-27 14:07:42 +0200

    Feat: Webshop page created with dummy data

---
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Webshop.jsx
@@ -0,0 +1,18 @@
+import {useState} from "react";
+import WebshopWineCard from "../Components/WebshopWineCard.jsx";
+import "../index.css";
+function Webshop() {
+    const [wine, setWine] = useState({name: "Chardonnay", year: 2010, price: 3200});
+    
+    return (
+        <div className="webshop">
+            <div className="webshop-container-wrapper">
+                <div className="webshop-container">
+                    <WebshopWineCard data={wine}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Webshop;
