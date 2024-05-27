cbbc145eaf48514b103551cb65938980840cf5f8
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-27 09:56:55 +0200

    feat: Cart model entity defined to hold items user wants to buy

---
+++ VineyardSite/VineyardSite/Model/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace VineyardSite.Model;
+
+public class Cart
+{
+    public int CartId { get; set; }
+    public ICollection<CartItem> CartItems { get; set; }
+    public string UserId { get; set; }
+}
