781b10137a36f08ef5bcb811bbf7fe1941357bd6
szoszo23 <szolnoki96@gmail.com>
2024-05-27 14:09:52 +0200

    feat: WebshopWineCard.jsx component created with dummy image

---
+++ VineyardSite/Client/VineyardSiteClient/src/Components/WebshopWineCard.jsx
@@ -0,0 +1,17 @@
+
+function WebshopWineCard(data) {
+    const wine = data.data;
+    return (
+        <div className={`${wine.name}-${wine.year}`}>
+            <img
+                src="https://media.nedigital.sg/fairprice/fpol/media/images/product/XL/10905268_XL1_20230510.jpg"
+                alt="data" width="200px" height="200px"/>
+            <h3>{wine.name}-{wine.year}</h3>
+            <h3>{wine.price} HUF</h3>
+            <button>Add to cart</button>
+            <button>More details</button>
+        </div>
+    )
+}
+
+export default WebshopWineCard;
