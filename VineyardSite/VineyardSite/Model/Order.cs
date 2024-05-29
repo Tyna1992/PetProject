f6ea187f1ec16b6654c3c25a3f68d98b2b16689c
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-29 13:56:54 +0200

    fix: In Order.cs the navigation property changed to WineVariant entity

--- VineyardSite/VineyardSite/Model/Order.cs
+++ VineyardSite/VineyardSite/Model/Order.cs
@@ -3,8 +3,8 @@
 public class Order
 {
     public Guid Id { get; set; }
-    public Wine Wine { get; set; }
-    public int WineId { get; set; }
+    public WineVariant WineVariant { get; set; }
+    public int WineVariantId { get; set; }
     public int Quantity { get; set; }
     public double TotalPrice { get; set; }
     public DateTime Date { get; set; }
