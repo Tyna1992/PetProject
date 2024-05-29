8c4b10da8ed3318f7efe31495d941af652de7175
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-29 13:51:18 +0200

    feat: Repository layer added for handling Cart operations

---
+++ VineyardSite/VineyardSite/Service/Repositories/CartRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardSite.Data;
+using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public class CartRepository : ICartRepository
+{
+    private readonly ApplicationDbContext _dataContext;
+   
+    
+    public CartRepository( ApplicationDbContext context)
+    {
+        
+        _dataContext = context;
+    }
+    
+    public async Task<IEnumerable<Cart>> GetAllCarts()
+    {
+        return await _dataContext.Carts.ToListAsync();
+    }
+
+    public async Task<Cart> GetCart(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddCart(Cart cart)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RemoveCart(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateCart(int id, Cart cart)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ VineyardSite/VineyardSite/Service/Repositories/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface ICartRepository
+{
+    Task<IEnumerable<Cart>> GetAllCarts(); 
+    Task<Cart> GetCart(int id);
+    Task AddCart(Cart cart);
+    Task RemoveCart(int id);
+    Task UpdateCart(int id, Cart cart);
+    
+}
