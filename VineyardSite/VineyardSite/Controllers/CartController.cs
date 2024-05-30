6b49f9d6166b3f14b8b0665d2d28ae29e5c754d7
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-30 10:47:34 +0200

    feat: PATCH and DELETE endpoint created to remove or update CartItems

--- VineyardSite/VineyardSite/Controllers/CartController.cs
+++ VineyardSite/VineyardSite/Controllers/CartController.cs
@@ -8,19 +8,22 @@ namespace VineyardSite.Controllers;
 public class CartController : ControllerBase
 {
     private readonly ICartItemRepository _cartItemRepository;
+    private readonly ICartRepository _cartRepository;
     
     
-    public CartController(ICartItemRepository cartItemRepository)
+    public CartController(ICartItemRepository cartItemRepository, ICartRepository cartRepository)
     {
         _cartItemRepository = cartItemRepository;
+        _cartRepository = cartRepository;
     }
+   
 
-    [HttpPost("AddCartItem/{drinkId}/{quantity}/{cartId}"), Authorize(Roles = "User, Admin")]
-    public async Task<IActionResult> AddCartItem(int drinkId, int quantity, int cartId)
+    [HttpPost("AddCartItem/{drinkId}/{quantity}/{userId}"), Authorize(Roles = "User, Admin")]
+    public async Task<IActionResult> AddCartItem(int drinkId, int quantity, string userId)
     {
         try
         {
-            await _cartItemRepository.AddCartItemAsync(drinkId, quantity, cartId);
+            await _cartItemRepository.AddCartItemAsync(drinkId, quantity, userId);
             return Ok("Item added to cart");
         }
         catch (Exception e)
@@ -30,4 +33,51 @@ public class CartController : ControllerBase
         }
     }
     
+    [HttpDelete("RemoveCartItem/{cartItemId}"), Authorize(Roles = "User, Admin")]
+    public async Task<IActionResult> RemoveCartItem(int cartItemId)
+    {
+        try
+        {
+            await _cartItemRepository.RemoveCartItemAsync(cartItemId);
+            return Ok("Item removed from cart");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, "Error removing item from cart");
+        }
+    }
+    
+    [HttpPatch("UpdateQuantity/{cartItemId}"), Authorize(Roles = "User, Admin")]
+    public async Task<IActionResult> UpdateQuantity(int cartItemId, int quantity)
+    {
+        try
+        {
+            var cartItem = await _cartItemRepository.GetCartItemAsync(cartItemId);
+            
+            cartItem.Quantity = quantity;
+            await _cartItemRepository.UpdateCartItemAsync(cartItem);
+            return Ok("Item quantity updated");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, "Error updating item quantity");
+        }
+    }
+
+    [HttpGet("GetCart/{cartId}"), Authorize(Roles = "User, Admin")]
+    public async Task<IActionResult> GetCart(int cartId)
+    {
+        try
+        {
+            var cartItems = await _cartItemRepository.GetCartItemsAsync(cartId);
+            return !cartItems.Any() ? Ok("Cart is empty") : Ok(cartItems);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, "Error getting cart items");
+        }
+    }
 }
