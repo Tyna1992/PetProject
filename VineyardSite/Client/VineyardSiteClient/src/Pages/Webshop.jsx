ab71ed8e66f475a421696c6310566fde8a4fd3d3
szoszo23 <szolnoki96@gmail.com>
2024-05-31 17:27:38 +0200

    feat: Inventory fetch implemented to the webshop.

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Webshop.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Webshop.jsx
@@ -1,14 +1,39 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import WebshopWineCard from "../Components/WebshopWineCard.jsx";
 import "../index.css";
 function Webshop() {
-    const [wine, setWine] = useState({name: "Chardonnay", year: 2010, price: 3200});
+    const [inventory, setInventory] = useState([]);
 
+    useEffect(()=>{
+        async function getInventory(){
+            try {
+                const response = await fetch("/api/Inventory/GetInventory",{
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+
+                })
+                if(response.ok)
+                {
+                    const data = await response.json();
+                    console.log(data);
+                    setInventory(data);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getInventory();
+    },[])
     return (
         <div className="webshop">
             <div className="webshop-container-wrapper">
                 <div className="webshop-container">
-                    <WebshopWineCard data={wine}/>
+                    {inventory.map((item) => (
+                        <WebshopWineCard data={item}/>
+                    ))}
                 </div>
             </div>
         </div>
