4ffe76e0fb011069683eb27e195e46a4b5d81fe3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-31 12:39:58 +0200

    fix: AddOrder method updated to get the items from Cart and update the inventory accordingly

--- VineyardSite/VineyardSite/Service/Repositories/OrderRepository.cs
+++ VineyardSite/VineyardSite/Service/Repositories/OrderRepository.cs
@@ -7,11 +7,14 @@ namespace VineyardSite.Service.Repositories;
 public class OrderRepository : IOrderRepository
 {
     private readonly ApplicationDbContext _context;
+    private readonly IInventoryRepository _inventoryRepository;
     
-    public OrderRepository(ApplicationDbContext context)
+    public OrderRepository(ApplicationDbContext context, IInventoryRepository inventoryRepository)
     {
         _context = context;
+        _inventoryRepository = inventoryRepository;
     }
+    
     
     public async Task<IEnumerable<Order>> GetAllOrders()
     {
@@ -24,32 +27,70 @@ public class OrderRepository : IOrderRepository
         return order;
     }
     
-    public async Task AddOrder(Order order)
+    public async Task<Order> AddOrder(string userId, OrderRequest orderRequest)
     {
+       
+        // Get the user's cart
+        var cart = await _context.Carts
+            .Include(c => c.CartItems)
+            .ThenInclude(ci => ci.WineVersion).Include(cart => cart.User)
+            .FirstOrDefaultAsync(c => c.UserId == userId);
+
+        var inventory = await _inventoryRepository.GetAllInventoryItems();
+        // Create a new order
+        var order = new Order
+        {
+            User = cart.User,
+            UserId = cart.UserId,
+            Date = DateTime.Now,
+            TotalPrice = cart.CartItems.Sum(ci => ci.WineVersion.Price * ci.Quantity),
+            Status = "Placed",
+            OrderItems = new List<OrderItem>(),
+            Email = cart.User.Email,
+            Address = cart.User.Address,
+            DeliveryType = orderRequest.DeliveryType,
+            PaymentType = orderRequest.PaymentType,
+            Notes = orderRequest.Notes
+            
+        };
+
+        // Copy cart items to order items
+        foreach (var cartItem in cart.CartItems)
+        {
+            var orderItem = new OrderItem
+            {
+                Quantity = cartItem.Quantity,
+                WineVariant = cartItem.WineVersion,
+                WineVariantId = cartItem.WineVariantId,
+                Order = order,
+                OrderId = order.Id
+            };
+           if(orderItem.Quantity < inventory.FirstOrDefault(i => i.WineVariantId == orderItem.WineVariantId)?.Quantity)
+            {
+                inventory.FirstOrDefault(i => i.WineVariantId == orderItem.WineVariantId).Quantity -= orderItem.Quantity;
+                _context.Inventory.Update(inventory.FirstOrDefault(i => i.WineVariantId == orderItem.WineVariantId));
+                order.OrderItems.Add(orderItem);
+            }
+            else
+            {
+                throw new Exception("Not enough stock");
+            }
+        }
+        
+        
+        // Save the order
         _context.Orders.Add(order);
         await _context.SaveChangesAsync();
+        
+        
+
+        // Clear the cart
+        _context.CartItems.RemoveRange(cart.CartItems);
+        await _context.SaveChangesAsync();
+        
+        return order;
     }
     
-    public async Task UpdateOrder(string id, Order order)
-    {
-        var orderToUpdate = await _context.Orders.FirstOrDefaultAsync(order => order.Id.ToString() == id);
-        if (orderToUpdate != null)
-        {
-            orderToUpdate.Date = order.Date;
-            orderToUpdate.TotalPrice = order.TotalPrice;
-            orderToUpdate.User = order.User;
-            orderToUpdate.WineVariant = order.WineVariant;
-            orderToUpdate.Address = order.Address;
-            orderToUpdate.Quantity = order.Quantity;
-            orderToUpdate.DeliveryType = order.DeliveryType;
-            orderToUpdate.PaymentType = order.PaymentType;
-            orderToUpdate.Status = order.Status;
-            orderToUpdate.Notes = order.Notes;
-            orderToUpdate.Email = order.Email;
-            _context.Orders.Update(orderToUpdate);
-            await _context.SaveChangesAsync();
-        }
-    }
     
     public async Task DeleteOrder(string id)
     {
