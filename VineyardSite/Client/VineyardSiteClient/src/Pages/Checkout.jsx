f7eddcac115806da0597c2dea902f0e660f73efa
szoszo23 <szolnoki96@gmail.com>
2024-05-31 15:55:44 +0200

    feat: Checkout page created.

---
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Checkout.jsx
@@ -0,0 +1,12 @@
+import {useEffect, useState} from "react";
+import "../index.css";
+
+function Checkout() {
+    
+    return (
+        <div>
+            <h1>Check out</h1>
+        </div>
+    )
+}
+export default Checkout;
