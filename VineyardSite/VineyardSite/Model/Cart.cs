f44ff2feca95c15e89bd77f3bbf05659c652f63b
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-05-31 12:33:15 +0200

    fix: JsonIgnore attribute added to User navigation property to prevent circular reference

--- VineyardSite/VineyardSite/Model/Cart.cs
+++ VineyardSite/VineyardSite/Model/Cart.cs
@@ -1,9 +1,12 @@
-﻿namespace VineyardSite.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace VineyardSite.Model;
 
 public class Cart
 {
     public int CartId { get; set; }
     public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
     public string UserId { get; set; }
+    [JsonIgnore]
     public User User { get; set; }
 }
