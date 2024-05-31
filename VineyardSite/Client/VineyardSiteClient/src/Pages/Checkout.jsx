ef293e7deeef58ca874c234700e4c462dff81124
szoszo23 <szolnoki96@gmail.com>
2024-05-31 16:07:29 +0200

    feat: OrderForm component implemented to the Checkout page.

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Checkout.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Checkout.jsx
@@ -1,11 +1,14 @@
 import {useEffect, useState} from "react";
 import "../index.css";
+import OrderForm from "../Components/OrderForm.jsx";
+
 
 function Checkout() {
     
     return (
         <div>
             <h1>Check out</h1>
+            <OrderForm />
         </div>
     )
 }
