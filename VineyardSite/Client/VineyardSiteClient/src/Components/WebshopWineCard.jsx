cd9da80e08b350bc7336258c2df762076751356a
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-04 13:21:06 +0200

    feat: Items can be added to the cart
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>
    Co-authored-by: Laszlo Prokaj <PrkjLaci@users.noreply.github.com>

--- VineyardSite/Client/VineyardSiteClient/src/Components/WebshopWineCard.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Components/WebshopWineCard.jsx
@@ -1,13 +1,49 @@
 import WineModal from "./WineModal.jsx";
-import {useState} from "react";
+import {useState, useContext} from "react";
+import notify from "../Utils/Notify.jsx";
+import { UserContext } from "./UserContext.jsx";
+
+
 function WebshopWineCard(data) {
     const wine = data.data;
     const [showModal, setShowModal] = useState(false);
+    const {user, setUser} = useContext(UserContext);
+
     function handleMoreDetails() {
         setShowModal(true)
     }
     function closeModal() {
         setShowModal(false);
+    }
+
+    async function addToCart(id)
+    {
+        
+        console.log(id);
+        const quantity = 1;
+        
+        try {
+            const response = await fetch(`/api/Cart/AddCartItem/${id}/${quantity}/${user}`,
+            {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({id, quantity, user})
+            })
+            if(response.ok)
+            {
+                notify("success", "Item added to cart")
+            }
+            else
+            {
+                notify("error", "Error adding item to cart")
+            }
+        } catch (error) {
+            console.error(error);
+            notify("error", "Error occured adding item to cart")
+        }
     }
 
     return (
@@ -17,7 +53,7 @@ function WebshopWineCard(data) {
                 alt="data" width="200px" height="200px"/>
             <h3>{wine.name}-{wine.year}</h3>
             <h3>{wine.price} HUF</h3>
-            <button>Add to cart</button>
+            <button onClick={()=> addToCart(wine.drinkId)}>Add to cart</button>
             <button onClick={handleMoreDetails}>More details</button>
             {showModal && <WineModal data={data} closeModal={closeModal}/>}
         </div>
