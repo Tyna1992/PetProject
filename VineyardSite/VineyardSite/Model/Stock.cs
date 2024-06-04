a5b800665c360c12b973707bb08857d0f929eff6
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-04 13:14:56 +0200

    fix: Description and Id properties added to Stock model
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>
    Co-authored-by: Laszlo Prokaj <PrkjLaci@users.noreply.github.com>

--- VineyardSite/VineyardSite/Model/Stock.cs
+++ VineyardSite/VineyardSite/Model/Stock.cs
@@ -2,10 +2,12 @@ namespace VineyardSite.Model;
 
 public class Stock
 {
+    public int DrinkId { get; set; }
     public string Name { get; set; }
     public int Year { get; set; }
     public double Price { get; set; }
     public double AlcoholContent { get; set; }
     public int Quantity { get; set; }
+    public string Description { get; set; }
 
 }
