fd4dac3a2036222637b26b8de8e4977fd9d3e581
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-04 14:54:20 +0200

    feat: OrderForm contains sendOrder method and form

--- VineyardSite/Client/VineyardSiteClient/src/Components/OrderForm.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Components/OrderForm.jsx
@@ -1,27 +1,74 @@
 import "../index.css";
+import { useContext } from "react";
+import { UserContext } from "./UserContext.jsx";
+import notify from "../Utils/Notify";
 
 function OrderForm() {
+    const {user, setUser} = useContext(UserContext);
+
+async function sendOrder(event)
+{
+    event.preventDefault();
+    const payment = event.target.payment.value;
+    const delivery = event.target.delivery.value;
+    const notes = event.target.notes.value; 
+    const userId = user.id;
+    console.log(user);
+
+    try {
+        const response= await fetch("/api/Order/AddOrder", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                UserId: userId,
+                PaymentType: payment,
+                DeliveryType: delivery,
+                Notes: notes
+            })
+            })
+            if(response.ok)
+            {
+                notify("Order sent", "success")
+            }
+    } catch (error) {
+        notify("Error sending order", "error")
+    }
+}
+
     return (
         <div className="orderForm">
             <div>
-                <form>
+                <form onSubmit={sendOrder}>
                     <label>
-                        Email:
+                        Payment
                     </label>
                     <br/>
-                    <input required type="email" name="orderEmail"/>
+                    <select name="payment">
+                        <option value="Card">Credit Card</option>
+                        <option value="Transfer">Bank transfer</option>
+                        <option value="CoD">Cash on delivery</option>
+                        <option value="CoP">Cash on pickup</option>
+                    </select>
                     <br/>
                     <label>
-                        Address:
+                        Delivery:
                     </label>
                     <br/>
-                    <input required type="text" name="orderAddress"/>
+                    <select name="delivery">
+                        <option value="Pickup">Pickup</option>
+                        <option value="GLS-point">GLS parcel point</option>
+                        <option value="GLS-home">GLS Home delivery</option>
+                        <option value="GLS-machine">GLS parcel machine</option>
+                    </select>
                     <br/>
                     <label>
-                        Quantity:
+                        Note to the seller:
                     </label>
                     <br/>
-                    <input required type="number" name="orderQuantity"/>
+                    <input type="text" name="notes"/>
                     <br/>
                     <button type="submit">Send Order</button>
                     <button type="button">Cancel</button>
@@ -29,6 +76,7 @@ function OrderForm() {
             </div>
         </div>
     )
+
 }
 
 export default OrderForm;
