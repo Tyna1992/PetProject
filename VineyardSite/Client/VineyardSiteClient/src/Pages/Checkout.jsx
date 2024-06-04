c8c56d579ac664a563a4e4bac0ef31a6bd73860b
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-04 13:22:01 +0200

    feat: Cheackout page displays the carts content
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>
    Co-authored-by: Laszlo Prokaj <PrkjLaci@users.noreply.github.com>

--- VineyardSite/Client/VineyardSiteClient/src/Pages/Checkout.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Pages/Checkout.jsx
@@ -1,14 +1,51 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext} from "react";
 import "../index.css";
 import OrderForm from "../Components/OrderForm.jsx";
+import { UserContext } from "../Components/UserContext.jsx";
 
 function Checkout() {
+    const {user, setUser} = useContext(UserContext);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() =>{
+        async function GetCart()
+        {
+            
+                const response = await fetch(`/api/Cart/GetCart/${user}`,{
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+                if(response.ok)
+                {
+                    const data = await response.json();
+                    setCart(data);
+                    console.log(data);
+                
+                }
+
+
+        }
+        GetCart();
+    },[])
 
     return (
         <div>
+            <h1>Cart</h1>
+            <div>
+                {cart.map((item, index) => (
+                    <div key={`${item.drinkName}-${index}`}>
+                        <h3>{item.drinkName}</h3>
+                        <h3>{item.quantity}</h3>
+                        
+                    </div>
+                ))}
             <h1>Check out</h1>
             <OrderForm />
         </div>
+    </div>
     )
 }
 export default Checkout;
