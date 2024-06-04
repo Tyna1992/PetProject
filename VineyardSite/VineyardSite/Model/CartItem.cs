a368628b872db8efc1eedc82020d3e1628ae39a8
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-04 13:17:17 +0200

    fix: CartItem Cart property gets JsonIgnore attribute to handle circular reference
    Co-authored-by: szoszo23 <szoszo23@users.noreply.github.com>
    Co-authored-by: Laszlo Prokaj <PrkjLaci@users.noreply.github.com>

--- VineyardSite/VineyardSite/Model/CartItem.cs
+++ VineyardSite/VineyardSite/Model/CartItem.cs
@@ -1,9 +1,12 @@
-﻿namespace VineyardSite.Model;
+﻿using System.Text.Json.Serialization;
+
+namespace VineyardSite.Model;
 
 public class CartItem
 {
     public int Id { get; set; }
     public int CartId { get; set; }
+    [JsonIgnore]
     public Cart Cart { get; set; }
     public int WineVariantId { get; set; }
     public WineVariant WineVersion { get; set; }
