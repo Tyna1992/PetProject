f4bc83f106cc0a781d3f13eb43b32b06f9bc114d
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-06-05 09:57:17 +0200

    The WineRepository interface and its implementation are added to the VineyardSite project. This allows for retrieving, adding, updating, and deleting wine data from the database.

---
+++ VineyardSite/VineyardSite/Service/Repositories/Wine/IWineRepository.cs
@@ -0,0 +1,12 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface IWineRepository
+{
+    Task<IEnumerable<Wine>> GetAllWine();
+    Task<Wine?> GetWineByName(string name);
+    Task AddWine(Wine wine);
+    Task UpdateWine(int id, Wine wine);
+    Task DeleteWine(int id);
+}
