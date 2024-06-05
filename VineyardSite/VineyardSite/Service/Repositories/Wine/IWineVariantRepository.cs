6f9c39fbe7e574924847055c86eae4b96cb60ec1
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-06-05 09:57:51 +0200

    The WineVariantRepository interface and its implementation are added to the VineyardSite project. This allows for retrieving, adding, updating, and deleting wine variant data from the database.

---
+++ VineyardSite/VineyardSite/Service/Repositories/Wine/IWineVariantRepository.cs
@@ -0,0 +1,14 @@
+﻿using VineyardSite.Model;
+
+namespace VineyardSite.Service.Repositories;
+
+public interface IWineVariantRepository
+{
+    Task<IEnumerable<WineVariant>> GetAllWineVariants();
+    Task<WineVariant> GetWineVariant(int id);
+    Task AddWineVariant(WineVariant wineVariant);
+    Task DeleteWineVariant(int id);
+    Task UpdateWineVariant(int id,WineVariant wineVariant);
+    Task<WineVariant> GetVariantByYear(int year,string name, double alcoholContent);
+
+}
