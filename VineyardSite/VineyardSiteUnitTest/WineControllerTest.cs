0167108cd64d8cd10c5203ef3756dab0ff511a9d
szoszo23 <szolnoki96@gmail.com>
2024-06-10 13:15:17 +0200

    test: Unit test added for AddWine method for failed added.

--- VineyardSite/VineyardSiteUnitTest/WineControllerTest.cs
+++ VineyardSite/VineyardSiteUnitTest/WineControllerTest.cs
@@ -12,7 +12,15 @@ public class WineControllerTest
 {
     private Mock<IWineRepository> _wineRepositoryMock;
     private WineController _wineController;
-
+    private Wine _testWine = new Wine
+    {
+        Id = 1,
+        Name = "testName",
+        Type = "testType",
+        Sweetness = "testSweetness",
+        Description = "testDescrition"
+    };
+    
     [SetUp]
     public void Setup()
     {
@@ -27,23 +35,29 @@ public class WineControllerTest
     }
 
     [Test]
-    public async Task AddWine_Success_ReturnsOk()
+    public async Task AddWine_SuccessAdded_ReturnsOk()
     {
-        Wine testWine = new Wine
-        {
-            Id = 1,
-            Name = "testName",
-            Type = "testType",
-            Sweetness = "testSweetness",
-            Description = "testDescrition"
-        };
 
         _wineRepositoryMock.Setup(repo => repo.AddWine(It.IsAny<Wine>())).Returns(Task.CompletedTask);
 
-        var result = await _wineController.AddWine(testWine);
+        var result = await _wineController.AddWine(_testWine);
         
         Assert.That(result, Is.InstanceOf<OkObjectResult>());
         var okResult = result as OkObjectResult;
-        Assert.That(okResult.Value, Is.EqualTo(testWine));
+        Assert.That(okResult.Value, Is.EqualTo(_testWine));
     }
+
+    [Test]
+
+    public async Task AddWine_FailsAdded_ReturnsBadRequest()
+    {
+        _wineRepositoryMock.Setup(repo => repo.AddWine(It.IsAny<Wine>())).ThrowsAsync(new Exception("Test Exception"));
+
+        var result = await _wineController.AddWine(_testWine);
+        
+        Assert.That(result, Is.InstanceOf<BadRequestObjectResult>());
+        var badRequestResult = result as BadRequestObjectResult;
+        Assert.That(badRequestResult.Value, Is.EqualTo("Test Exception"));
+    }
+    
 }
