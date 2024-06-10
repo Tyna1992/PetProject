e583b3d13ebb7f6365ad472da5e7a0d3eb0b3a11
szoszo23 <szolnoki96@gmail.com>
2024-06-10 13:03:11 +0200

    test: Unit test added for AddWine method for successfully added wine

--- VineyardSite/VineyardSiteUnitTest/WineControllerTest.cs
+++ VineyardSite/VineyardSiteUnitTest/WineControllerTest.cs
@@ -3,18 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using VineyardSite.Contracts;
 using VineyardSite.Controllers;
+using VineyardSite.Model;
 using VineyardSite.Service.Repositories;
 
 namespace VineyardSiteUnitTest;
 
 public class WineControllerTest
 {
-    private readonly Mock<IWineRepository> _wineRepositoryMock;
+    private Mock<IWineRepository> _wineRepositoryMock;
     private WineController _wineController;
 
     [SetUp]
     public void Setup()
     {
+        _wineRepositoryMock = new Mock<IWineRepository>();
         _wineController = new WineController(_wineRepositoryMock.Object)
         {
             ControllerContext = new ControllerContext()
@@ -22,5 +24,26 @@ public class WineControllerTest
                 HttpContext = new DefaultHttpContext()
             }
         };
+    }
+
+    [Test]
+    public async Task AddWine_Success_ReturnsOk()
+    {
+        Wine testWine = new Wine
+        {
+            Id = 1,
+            Name = "testName",
+            Type = "testType",
+            Sweetness = "testSweetness",
+            Description = "testDescrition"
+        };
+
+        _wineRepositoryMock.Setup(repo => repo.AddWine(It.IsAny<Wine>())).Returns(Task.CompletedTask);
+
+        var result = await _wineController.AddWine(testWine);
+        
+        Assert.That(result, Is.InstanceOf<OkObjectResult>());
+        var okResult = result as OkObjectResult;
+        Assert.That(okResult.Value, Is.EqualTo(testWine));
     }
 }
