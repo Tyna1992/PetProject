82a8deeefccb95052b23afd061787c5b85d05c67
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-06-17 10:24:14 +0200

    feat: email sending interface implemented, using Gmail SMTP host

---
+++ VineyardSite/VineyardSite/Service/EmailService/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Net;
+using System.Net.Mail;
+
+namespace VineyardSite.Service.EmailService;
+
+public class EmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string message)
+    {
+        var client = new SmtpClient("smtp.gmail.com", 587)
+        {
+            EnableSsl = true,
+            UseDefaultCredentials = false,
+            Credentials = new NetworkCredential("csobancibormanufakturawebshop@gmail.com", "bhwv pgtz mmjb rzkv")
+
+        };
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("csobancibormanufakturawebshop@gmail.com") ,
+            Subject = subject,
+            Body = message,
+            IsBodyHtml = false
+        };
+        
+        mailMessage.To.Add(email);
+        return client.SendMailAsync(mailMessage);
+    }
+}
