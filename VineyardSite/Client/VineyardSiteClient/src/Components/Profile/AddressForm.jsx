caf8cd64cc7ccd4ea0696813b361d7a52476f969
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-06-17 12:43:23 +0200

    Refactor AddAddress component and form handling

--- VineyardSite/Client/VineyardSiteClient/src/Components/Profile/AddressForm.jsx
+++ VineyardSite/Client/VineyardSiteClient/src/Components/Profile/AddressForm.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
-const AddressForm = ({
-  handleSubmit,
-  address,
-  handleChange,
-  haveAddress,
-  handleUpdateSubmit,
-  editingAddress,
-  setEditingAddress,
-  isUpdated,
-  setIsUpdated,
-  modify,
-  setModify
-}) => {
-
-  return (editingAddress === false && modify === false && isUpdated === false) ||
-    (editingAddress === true && modify === false && isUpdated === false) ? (
+const AddressForm = ({ handleSubmit, address, handleChange, haveAddress }) => {
+  return (
     <div className="form-container">
       <div className="editModal">
         <form onSubmit={handleSubmit}>
@@ -24,88 +10,9 @@ const AddressForm = ({
             <input
               type="text"
               name="street"
-              disabled={haveAddress}
               value={address.street}
               onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>House Number</label>
-            <input
-              type="text"
-              name="houseNumber"
-              disabled={haveAddress}
-              value={address.houseNumber}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Zip Code</label>
-            <input
-              type="text"
-              name="zipCode"
-              disabled={haveAddress}
-              value={address.zipCode}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>City</label>
-            <input
-              type="text"
-              name="city"
-              disabled={haveAddress}
-              value={address.city}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Country</label>
-            <input
-              type="text"
-              name="country"
-              disabled={haveAddress}
-              value={address.country}
-              onChange={handleChange}
-            />
-          </div>
-          <button
-            hidden={haveAddress}
-            type="submit"
-            className="button"
-            onClick={() => {
-              setEditingAddress(true);
-            }}
-          >
-            Submit
-          </button>
-        </form>
-        <button
-          hidden={!haveAddress}
-          style={{
-            marginBottom: "100px",
-          }}
-          onClick={() => {setModify(true); setIsUpdated(true)}}
-        >
-          Modify
-        </button>
-      </div>
-    </div>
-  ) : (
-    <div className="form-container">
-      <div className="editModal">
-        <form onSubmit={handleUpdateSubmit}>
-          <div className="form-group">
-            <label>Street</label>
-            <input
-              type="text"
-              name="street"
-              value={address.street}
-              onChange={handleChange}
+              required
             />
           </div>
 
@@ -116,6 +23,7 @@ const AddressForm = ({
               name="houseNumber"
               value={address.houseNumber}
               onChange={handleChange}
+              required
             />
           </div>
 
@@ -126,6 +34,7 @@ const AddressForm = ({
               name="zipCode"
               value={address.zipCode}
               onChange={handleChange}
+              required
             />
           </div>
 
@@ -136,6 +45,7 @@ const AddressForm = ({
               name="city"
               value={address.city}
               onChange={handleChange}
+              required
             />
           </div>
 
@@ -146,25 +56,14 @@ const AddressForm = ({
               name="country"
               value={address.country}
               onChange={handleChange}
+              required
             />
           </div>
 
-          <button
-            type="submit"
-            className="button"
-          >
-            Save
+          <button type="submit" className="button">
+            {haveAddress ? "Update" : "Submit"}
           </button>
         </form>
-        <button
-          className="button"
-          onClick={() => {
-            setEditingAddress(false);
-            setModify(false);
-          }}
-        >
-          Cancel
-        </button>
       </div>
     </div>
   );
