bb8c2df61bbb9fcf396d6e6b6178f17a54411b70
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-15 10:04:37 +0200

    Add GetOrdersByUserId method to IOrderRepository and OrderRepository

--- VineyardSite/VineyardSite/Service/Repositories/IOrderRepository.cs
+++ VineyardSite/VineyardSite/Service/Repositories/IOrderRepository.cs
@@ -7,6 +7,7 @@ public interface IOrderRepository
     Task<IEnumerable<Order>> GetAllOrders();
     Task<Order> GetOrderById(string id);
     Task<Order> AddOrder(string userId, OrderRequest orderRequest);
+    Task<ICollection<Order>> GetOrdersByUserId(string userId);
     
     Task DeleteOrder(string id);
 }
