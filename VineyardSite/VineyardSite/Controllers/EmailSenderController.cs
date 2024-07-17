59c6628e4fde5875a0462e0229a3ff3594522ef1
Laszlo Prokaj <prokaj.laszlo96@gmail.com>
2024-07-17 13:29:24 +0200

    Add EmailSenderController for sending user emails

---
+++ VineyardSite/VineyardSite/Controllers/EmailSenderController.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using VineyardSite.Contracts;
+using VineyardSite.Service.EmailService;
+
+namespace VineyardSite.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+
+public class EmailSenderController : ControllerBase
+{
+    private readonly IEmailSender _emailSender;
+    
+    public EmailSenderController(IEmailSender emailSender)
+    {
+        _emailSender = emailSender;
+    }
+
+    [HttpPost("sendEmail")]
+    public async Task<IActionResult> SendUserEmail([FromBody] UserEmailRequest request)
+    {
+        try
+        {
+            var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+            Console.WriteLine(userEmail);
+            var userName = User.FindFirst(ClaimTypes.Name)?.Value;
+            Console.WriteLine(userName);
+            
+            if (userEmail != null && userName != null)
+            {
+                await _emailSender.SendUserEmailAsync(userEmail, request.Subject, userName, request.Message);
+            }
+            else
+            {
+                return BadRequest("User email or name not found.");
+            }
+
+            return Ok("Email sent successfully.");
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+}
