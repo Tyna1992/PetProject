8add0a7f9740a920f8394c8f91c00b717af396b3
Takács Tímea Erika <timea.kaposvolgyi@gmail.com>
2024-07-18 10:39:18 +0200

    feat: New method for sending request email added to the interface

--- VineyardSite/VineyardSite/Service/EmailService/IEmailSender.cs
+++ VineyardSite/VineyardSite/Service/EmailService/IEmailSender.cs
@@ -5,6 +5,7 @@ public interface IEmailSender
     Task SendEmailAsync(string email, string subject, string message);
     Task SendSignUpEmailAsync(string email, string username);
     Task SendUserEmailAsync(string email, string subject, string userName, string message);
+    Task SendRequestEmailAsync(string email, string subject, string name, string phone, int people, string date, string message);
 }
 
     
