dcbef366f295b65f1f03504613fbae4cdc42009c
szoszo23 <szolnoki96@gmail.com>
2024-09-03 13:30:00 +0200

    feat: Register form implemented

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/register/register.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/register/register.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
 
+  onSubmit() {
+    const loginData = {
+      username: this.username.value,
+      email: this.email.value,
+      password: this.password.value
+    }
+    console.log('Form Submitted', loginData);
+  }
 }
