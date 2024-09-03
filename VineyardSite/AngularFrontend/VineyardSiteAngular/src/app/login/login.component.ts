247f08e7d9fb6f9df8fe2f3ce53f06246c08ba20
szoszo23 <szolnoki96@gmail.com>
2024-09-03 13:28:59 +0200

    feat: Login form implemented

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/login.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = new FormControl('');
+  password = new FormControl('');
+
+  onSubmit() {
+    const loginData = {
+      username: this.username.value,
+      password: this.password.value
+    }
+    console.log('Form Submitted', loginData);
+    console.log('Form Submitted');
+    console.log('Username:', this.username.value);
+    console.log('Password:', this.password.value);
+  }
 
 }
