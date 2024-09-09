11f2cebd86ecd88e86c93ee1a6004f5d0094e9ea
szoszo23 <szolnoki96@gmail.com>
2024-09-09 09:13:41 +0200

    feat: Implemented auth service for login.

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/auth.service.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = '/api/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { username: string; password: string }): Observable<any> {
+    return this.http.post<any>(this.apiUrl, credentials).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.token);
+      }),
+      catchError(error => {
+        console.error('Login error', error);
+        return of(null);
+      })
+    );
+  }
+
 }
