00b11cdc724e8c044349d8157021d07442fc3766
szoszo23 <szolnoki96@gmail.com>
2024-09-09 09:14:29 +0200

    feat: Login component updated with API call.

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/login.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/login.component.ts
@@ -1,26 +1,38 @@
 import { Component } from '@angular/core';
-import { FormControl,ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder,FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username = new FormControl('');
-  password = new FormControl('');
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+  
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
 
-  onSubmit() {
-    const loginData = {
-      username: this.username.value,
-      password: this.password.value
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (response) => {
+          console.log('u are logged in')
+        },
+        error: (err) => {
+          this.errorMessage = 'Invalid username or password';
+        }
+      });
     }
-    console.log('Form Submitted', loginData);
-    console.log('Form Submitted');
-    console.log('Username:', this.username.value);
-    console.log('Password:', this.password.value);
   }
 
 }
