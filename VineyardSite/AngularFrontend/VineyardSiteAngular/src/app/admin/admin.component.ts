343b474dd2a936966bc0f7c2d37fb04f9185d77a
szoszo23 <szolnoki96@gmail.com>
2024-09-12 14:47:01 +0200

    feat: import components to the admin component

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/admin/admin.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/admin/admin.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { WineFormComponent } from './wineForm/wineForm.component';
+import { WineVariantFormComponent } from './wineVariantForm/wineVariantForm.component';
+import { InventoryFormComponent } from './inventory/inventory-form/inventory-form.component';
+import { InventoryDetailsFormComponent } from './inventory/inventory-details-form/inventory-details-form.component';
+import { InventoryTableComponent } from './inventory-table/inventory-table.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [WineFormComponent, WineVariantFormComponent, InventoryFormComponent, InventoryDetailsFormComponent, InventoryTableComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
