aef02526f4bb168dc8470a98981e11ca97d4e236
szoszo23 <szolnoki96@gmail.com>
2024-09-12 14:45:07 +0200

    feat: Interfaces created for wine data

---
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/Models/InventoryStock.ts
@@ -0,0 +1,9 @@
+export interface IInventoryStock {
+    DrinkId: number;
+    Name: string;
+    Year: number;
+    Price: number;
+    AlcoholContent: number;
+    Quantity: number;
+    Description: string;
+  }
