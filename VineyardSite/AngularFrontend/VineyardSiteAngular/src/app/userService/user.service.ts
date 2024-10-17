70086a88fdf89be9ecd9639ae4701eb980564323
szoszo23 <szolnoki96@gmail.com>
2024-10-17 16:26:52 +0200

    feat: User service updated with login check

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/userService/user.service.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/userService/user.service.ts
@@ -1,15 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private isLoggedIn = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) { }
 
   whoAmI(): Observable<any>{ 
     return this.http.get('/api/Auth/WhoAmI');
+  }
+
+  isLoggedIn$(): Observable<boolean> {
+    return this.isLoggedIn.asObservable();
+  }
+
+  setLoginStatus(status: boolean) {
+    this.isLoggedIn.next(status);
+  }
+
+  login() {
+    this.setLoginStatus(true);
+  }
+
+  logout() {
+    this.setLoginStatus(false);
+  }
+
+  getUserDetails(id: any): Observable<any>{
+    return this.http.get(`/api/User/GetUserDetails/${id}`);
+  }
+
+  getAddress(id: any): Observable<any> {
+    return this.http.get(`/api/User/GetAllAddress/${id}`);
+  }
+
+  addPrimaryAddress(id: any, data: any): Observable<any> {
+    console.log('body: ', data)
+    return this.http.post(`/api/User/AddAddress/${id}`, data);
   }
 }
