7f54d6a891d1f6851d68d4f0736940186814cc57
szoszo23 <szolnoki96@gmail.com>
2024-10-17 16:19:41 +0200

    feat: Login check added to the app component

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/app.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ReactiveFormsModule, CommonModule],
   templateUrl: './navbar/navbar.component.html',
   styleUrl: './navbar/navbar.component.css'
 })
 export class AppComponent {
   title = 'VineyardSiteAngular';
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$();
+  }
 }
