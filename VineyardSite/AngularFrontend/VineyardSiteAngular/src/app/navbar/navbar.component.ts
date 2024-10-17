001018bc1e5e5eb774e45b29db8510f4a5288e95
szoszo23 <szolnoki96@gmail.com>
2024-10-17 16:24:23 +0200

    feat: navbar component updated with login check

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/navbar/navbar.component.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/navbar/navbar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../login/auth.service';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn$: Observable<boolean>;
 
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn$();
+  }
+
+  ngOnInit(): void {
+    this.authService.loginSet();
+  }
 }
