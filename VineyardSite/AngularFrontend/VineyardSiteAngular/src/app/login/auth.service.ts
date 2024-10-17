06db662bce1a7b09bc2ca495f4723c5e9efadc0c
szoszo23 <szolnoki96@gmail.com>
2024-10-17 16:22:16 +0200

    feat: Add login check to auth service

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/auth.service.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/login/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, of, BehaviorSubject } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
@@ -9,6 +9,7 @@ import { tap, catchError } from 'rxjs/operators';
 export class AuthService {
 
   private apiUrl = '/api/auth/login';
+  private isLoggedIn = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +17,7 @@ export class AuthService {
     return this.http.post<any>(this.apiUrl, credentials).pipe(
       tap(response => {
         localStorage.setItem('token', response.token);
+        this.setLoginStatus(true);
       }),
       catchError(error => {
         console.error('Login error', error);
@@ -24,4 +26,23 @@ export class AuthService {
     );
   }
 
+  loginSet(){
+    this.setLoginStatus(true);
+  }
+
+  isLoggedIn$(): Observable<boolean> {
+    return this.isLoggedIn.asObservable();
+  }
+
+  setLoginStatus(status: boolean) {
+    this.isLoggedIn.next(status);
+  }
+
+  logout() {
+    this.setLoginStatus(false);
+  }
+
+  isAuthenticated(): boolean {
+    return this.isLoggedIn.value;
+  }
 }
