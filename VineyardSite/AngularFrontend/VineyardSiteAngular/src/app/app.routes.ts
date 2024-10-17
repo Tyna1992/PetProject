30494e1fb0cb62effaf57bdd4930224befccfb78
szoszo23 <szolnoki96@gmail.com>
2024-10-17 16:20:25 +0200

    feat: Routes updated.

--- VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/app.routes.ts
+++ VineyardSite/AngularFrontend/VineyardSiteAngular/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { AdminComponent } from './admin/admin.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OrderComponent } from './order/order.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
@@ -18,5 +20,7 @@ export const routes: Routes = [
     {path: "about", component: AboutComponent},
     {path: "contact", component: ContactComponent},
     {path: "checkout", component: CheckoutComponent},
-    {path: "admin", component: AdminComponent}
+    {path: "admin", component: AdminComponent},
+    {path: "profile", component: ProfileComponent},
+    {path: "order", component: OrderComponent}
 ];
